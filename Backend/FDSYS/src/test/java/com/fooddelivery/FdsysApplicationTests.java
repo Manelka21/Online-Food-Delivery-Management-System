4959d44af3f4304d7bb333c2334f69ba87a71501
Manelka21 <pasindumanelka@gmail.com>
2026-05-06 10:04:40 +0530

    initial

---
+++ Backend/FDSYS/src/test/java/com/fooddelivery/FdsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fooddelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdsysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
